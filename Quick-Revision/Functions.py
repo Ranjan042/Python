a3565e22511d09a22444de117e70371f616b46b7
ranjan <chauhanranjan8978@gmail.com>
2026-09-06 21:20:51 +0530

    Function Revision

---
+++ Quick-Revision/Functions.py
@@ -0,0 +1,27 @@
+# def greet():
+#     print("Hello")
+
+
+# def greetWithName(name):
+#     print(f"Hello {name}")
+
+
+# greet()
+# greetWithName("Ranjan")
+
+def add(a,b):
+    print(a+b)
+
+add(10,20)
+
+def introduce(name,age):
+    print(f"Hello my name is {name} and my age is {age}")
+
+introduce("ranjan",20)
+
+def greet(name="Ranjan"):
+    print(f"Hello {name}")
+
+greet()
+greet("mithun")
+
